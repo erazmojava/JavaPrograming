1d0eb28e99344663198c565969f53004ddbed715
Osman Residovic <you@example.com>
2017-05-20 19:04:44 +0200

    added explain for Zadatak8

--- src/zadatak8/Zadatak8.java
+++ src/zadatak8/Zadatak8.java
@@ -11,27 +11,30 @@ public class Zadatak8 {
         Scanner s = new Scanner(System.in);
         while (s.hasNext()){
             numList.add(s.nextInt());
-            if (numList.size() == 5){
+            if (numList.size() == 5){ // kad je unešeno 5 brojeva scanner se zatvara
                 break;
             }
         }
 
 
-        ArrayList<Integer> tempList = new ArrayList<>();
-        tempList.add(numList.get(0));
+        ArrayList<Integer> tempList = new ArrayList<>(); //pravimo novu sortiranu listu
+        tempList.add(numList.get(0)); //dodajemo prvi broj u sortiranu listu
         for (int i = 1; i <numList.size(); i++){
-            int tempSize = tempList.size();
+            int tempSize = tempList.size(); // pamtimo belicinu sortirane liste
            for (int j = 0; j< tempList.size();j++){
-               if (numList.get(i) > tempList.get(j)){
-                   tempList.add(j, numList.get(i));
-                   break;
+               if (numList.get(i) > tempList.get(j)){ //ako je novi broj veci od brojeva
+                   // u sortiranoj listi dodajmo ga ispred tog broja u sortiranoj listi
+                   tempList.add(j, numList.get(i)); //dodaje ispred trenutnog broja u sortiranoj listi
+                                                    //ako je veci od tog broja
+                   break; //prekida petlju jer dodao broj da ne bi dodavao duplikate
                }
            }
-           if (tempList.size() == tempSize){
+           if (tempList.size() == tempSize){ //ako se nista nije desilo u drugoj for petlji
+                                             //dodajmo na kraj sortirane liste jer je trenutni broj najmanji
                tempList.add(numList.get(i));
            }
         }
-        System.out.println(tempList.toString());
+        System.out.println(tempList.toString()); //ispis liste koristi se metod na listi toString();
     }
 
 }

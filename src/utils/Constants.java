e92caef7ab4c804fef7ffd49cdbcd2c51be943ea
Osman Residovic <you@example.com>
2017-05-23 20:25:02 +0200

    some stuff

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -1,5 +1,7 @@
 package utils;
 
+import zadatak9.Auto;
+
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -14,5 +16,18 @@ public class Constants {
             numberList.add(r.nextInt(1000) + 1);
         }
         return numberList;
+    }
+    public static ArrayList<Auto> getAutoList() {
+        ArrayList<Auto> autos = new ArrayList();
+        autos.add(new Auto("Mazda", 2014, 5, "red", "automatic", "Japan"));
+        autos.add(new Auto("Alfa", 2000, 4, "blue", "manual", "Italy"));
+        autos.add(new Auto("Golf", 2009, 5, "green", "automatic", "Germany"));
+        autos.add(new Auto("Mercedes", 2016, 6, "blue", "manual", "Germany"));
+        autos.add(new Auto("Lancia", 2002, 4, "red", "automatic", "Italy"));
+        autos.add(new Auto("Yugo", 1990, 4, "green", "manual", "Serbia"));
+        autos.add(new Auto("Toyota", 2006, 5, "blue", "manual", "Japan"));
+        autos.add(new Auto("Lexus", 2010, 4, "red", "automatic", "China"));
+        autos.add(new Auto("Jaguar", 2017, 8, "blue", "automatic", "Great Brittany"));
+        return autos;
     }
 }
---
+++ src/zadatak9/Zadatak9.java
@@ -0,0 +1,54 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package zadatak9;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import utils.Constants;
+
+import javax.crypto.AEADBadTagException;
+
+public class Zadatak9 {
+    public Zadatak9() {
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Auto> autos = Constants.getAutoList();
+        ArrayList<Auto> sortAutos = new ArrayList();
+
+        for(int i = 1; i < autos.size(); ++i) {
+            int size = sortAutos.size();
+
+            for(int j = 0; j < sortAutos.size(); ++j) {
+                if((autos.get(i)).getYear() > (sortAutos.get(j)).getYear()) {
+                    sortAutos.add(j, autos.get(i));
+                    break;
+                }
+            }
+
+            if(sortAutos.size() == size) {
+                sortAutos.add(autos.get(i));
+            }
+        }
+
+        System.out.println("Sortirana auta po godini: ");
+        for (Auto auto : sortAutos){
+            System.out.println(auto.getYear() +"  "+ auto.getName());
+        }
+
+        ArrayList<Auto> redAutos = new ArrayList<>();
+        System.out.println();
+        System.out.println("Crvena auta: ");
+        for (Auto auto : autos){
+           if (auto.getColor().equals("red")){
+               redAutos.add(auto);
+           }
+        }
+        for (Auto auto : redAutos){
+            System.out.println(auto.getColor() +"  "+ auto.getName());
+        }
+    }
+}

645d21b05aa3c8efae9f82f06f0be8b59234b541
Ervin <ervin@gmail.com>
2017-05-29 18:09:49 +0200

    zadatak12 - solved

--- src/zadatak12/Zadatak12.java
+++ src/zadatak12/Zadatak12.java
@@ -4,16 +4,58 @@ import utils.Constants;
 
 import java.util.ArrayList;
 
-/**
- * Created by osman on 5/27/17.
- */
-public class Zadatak12 {
-    public static void main(String [] args){
-       School school = Constants.getSchool();
-       //na klasi student ima lista studenata ima lista classroom-a
-        System.out.println("Zadatak12 a)");
-        for (Student student : school.getStudents()){
 
-       }
-    }
-}
+public class Zadatak12 {
+    public static void main(String[] args) {
+        School school = Constants.getSchool();
+        ArrayList<Classroom> sortStudent = new ArrayList<>();
+        ArrayList<Student> sortStudentClass = new ArrayList<>();
+        System.out.println("Zadatak12 a)");
+        for (Classroom classroom : school.getClassrooms()) {
+            sortStudent.add(classroom);
+            for (Student student : school.getStudents()) {
+                sortStudentClass.add(student);
+            }
+        }
+        for (Student student : school.getStudents()) {
+            if (student.getClassroom_id() == 1) {
+                System.out.println("First" + " " + student.getName());
+
+
+            }
+        }
+
+        System.out.println(" ");
+        System.out.println("Zadatak12 b)");
+        ArrayList<Student> sortUlica = new ArrayList<>();
+        ArrayList<Place> sortPlace = new ArrayList<>();
+        for (Student student : school.getStudents()) {
+            sortUlica.add(student);
+        }
+
+        for (Student student : sortUlica) {
+            if (student.getPlace().getStreet().equals("Ulica17")) {
+                System.out.println(student.getName() + " " + student.getPlace().getStreet());
+            }
+        }
+        System.out.println(" ");
+        System.out.println("Zadatak12 c)");
+        for (Classroom classroom : school.getClassrooms()) {
+            if (classroom.getStudent_count() > 3) {
+                System.out.println(classroom.getName() + " " + "count: " + classroom.getStudent_count());
+                for (Student student : school.getStudents()) {
+                    if (student.getClassroom_id() == classroom.getClassroom_id()) {
+                        System.out.println(student.getName());
+                    }
+                }
+            }
+        }
+    }}
+
+
+
+
+
+
+
+

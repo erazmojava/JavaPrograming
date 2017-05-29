728ec64d7f8bfb4797069781e724c81fd2e9e4b8
Omar <zajko194@hotmail.com>
2017-05-29 16:36:47 +0200

    Zadatak12

--- src/zadatak12/Zadatak12.java
+++ src/zadatak12/Zadatak12.java
@@ -4,16 +4,43 @@ import utils.Constants;
 
 import java.util.ArrayList;
 
-/**
- * Created by osman on 5/27/17.
- */
 public class Zadatak12 {
-    public static void main(String [] args){
-       School school = Constants.getSchool();
-       //na klasi student ima lista studenata ima lista classroom-a
+    public static void main(String[] args) {
+        School school = Constants.getSchool();      //na klasi student ima lista studenata ima lista classroom-a
         System.out.println("Zadatak12 a)");
-        for (Student student : school.getStudents()){
+        ArrayList<Classroom> classrooms = Constants.getSchool().getClassrooms();
+        ArrayList<Student> students = Constants.getSchool().getStudents();
 
-       }
+        for (Classroom classroom : classrooms) {
+            if (classroom.getName().equals("First")) {
+                for (Student student : students) {
+                    if (student.getClassroom_id() == 1) {
+                        System.out.println(student.getName() + "  " + classroom.getName());
+
+
+                    }
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("Zadatak12 b)");
+
+
+        for (Student student : students) {
+            if (student.getPlace().getStreet().equals("Ulica17")) {
+                System.out.println(student.getName() + "  " + student.getPlace().getStreet());
+
+            }
+        }
+        System.out.println();
+        System.out.println("Zadatak12 c)");
+
+
+        for (Classroom classroom : classrooms) {
+            if (classroom.getStudent_count() > 3) ;{
+                System.out.println(classroom.getName() + "   " + classroom.getStudent_count());
+
+            }
+        }
     }
 }

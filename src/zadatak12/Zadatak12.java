52adc182e92e0ccee6c9d18fdd33220f73eb8c62
Osman Residovic <you@example.com>
2017-05-29 19:49:26 +0200

    zadatak12-solved

--- src/zadatak12/Zadatak12.java
+++ src/zadatak12/Zadatak12.java
@@ -10,10 +10,38 @@ import java.util.ArrayList;
 public class Zadatak12 {
     public static void main(String [] args){
        School school = Constants.getSchool();
-       //na klasi student ima lista studenata ima lista classroom-a
+       /////////////////////////////////////////////////////////////////////////////////////////
         System.out.println("Zadatak12 a)");
         for (Student student : school.getStudents()){
+           for (Classroom classroom : school.getClassrooms()){
+               if (classroom.getName().equals("First") && student.getClassroom_id() == classroom.getClassroom_id()){
+                   System.out.println(classroom.getName()+ "  " + student.getName());
+               }
+           }
 
        }
+       ///////////////////////////////////////////////////////////////////////////////////////////
+       System.out.println();
+       System.out.println("Zadatak12 b)");
+        for (Student student : school.getStudents()){
+           if (student.getPlace().getStreet().equals("Ulica17")){
+               System.out.println(student.getName() +"  " + student.getPlace().getStreet());
+           }
+        }
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        System.out.println();
+        System.out.print("Zadatak12 c)");
+        for (Classroom classroom : school.getClassrooms()){
+            if (classroom.getStudent_count() > 3){
+                System.out.println();
+                System.out.println(classroom.getName() +"  count: "+classroom.getStudent_count());
+                for (Student student : school.getStudents()){
+                    if (student.getClassroom_id() == classroom.getClassroom_id()){
+                        System.out.println(student.getName());
+                    }
+                }
+            }
+        }
+        //////////////////////////////////////////////////////////////////////////////////////////
     }
 }

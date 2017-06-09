66c0ef3e0f421e1c637acd185f09b66fe0d2f16d
Mahir <Mahir>
2017-06-09 22:56:45 +0200

    mahir

--- src/zadatak16/Zadatak16.java
+++ src/zadatak16/Zadatak16.java
@@ -16,7 +16,7 @@ public class Zadatak16 {
         listaPersona.add(new Developer("Hasan", "Maliko", 19, "female", new Place(75000, "Ulica2", "Lukavac", "Bih"), 31, "Angular Developer", 3, 1000));
         listaPersona.add(new Employee("Tito", "Broz", 20, "male", new Place(75300, "Ulica3", "Lukavac", "BiH"), "Marketing", 400, "Worker"));
         listaPersona.add(new Student("Amir", "Hasanovic", 33, "male", new Place(75020, "Ulica5", "Mostar", "BiH"), 23223, new School("Škola34"), new Place(75400, "Ulica54", "Modriča", "Bih"), new Classroom("Učionica1", 25, 2366)));
-        listaPersona.add(new Developer("Malik", "Mali", 90, "male", new Place(64000, "Ulica8", "Čapljina", "BiH"), 23, "Menadžer", 8, 1200));
+        listaPersona.add(new Developer("Caco", "Mali", 90, "male", new Place(64000, "Ulica8", "Čapljina", "BiH"), 23, "Menadžer", 8, 1200));
 
         for (Object person : listaPersona) {
             if (person instanceof Student) {
@@ -30,6 +30,7 @@ public class Zadatak16 {
             if (person instanceof Developer) {
                 Developer developer = (Developer) person;
                 developer.onSalaryUpdate(100);
+                System.out.println(developer.getName()+ " " + developer.getSalary());
 
 
                 Person person1 = (Person) person;
@@ -45,7 +46,7 @@ public class Zadatak16 {
             if (person instanceof Employee){
                 Employee employee = (Employee)person;
                 if (employee.getSalary()<1000 && employee.getSex().equals("male")){
-                    System.out.println(employee.getName());
+                    System.out.println(employee.getName()+ " " + employee.getSurname() + " " + employee.getSalary());
                 }
             }
         }

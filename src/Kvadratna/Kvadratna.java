7b238a33d79cf0c0124da85216bbbba8b2dcd9d9
omar <zajko194@hotmail.com>
2017-06-23 19:04:10 +0200

    Kvadtratna jednacina

---
+++ src/Kvadratna/Kvadratna.java
@@ -0,0 +1,60 @@
+package Kvadratna;
+
+import java.util.Scanner;
+
+/**
+  Created by Omar on 22.06.2017..
+ */
+public class Kvadratna {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Unesite tri broja: ");
+        int i = 0;
+        double a = 0;
+        double b = 0;
+        double c = 0;
+        double d;
+        double x1 = 0;
+        double x2 = 0;
+
+
+        System.out.printf("Prvi broj (a): ");
+        while (s.hasNext()) {
+            if (i == 0) {
+                a = s.nextDouble();
+                System.out.printf("Drugi broj (b): ");
+            }
+            if (i == 1) {
+                b = s.nextDouble();
+                System.out.printf("Treci broj (c): ");
+            }
+            if (i == 2) {
+                c = s.nextDouble();
+                break;
+            }
+
+            i++;
+        }
+
+
+        d = ((b * b) - 4 * a * c);
+
+        if (d < 0) {
+            System.out.println("Rjesenja funkcije nisu realna");
+        }
+        if (d == 0) {
+            x1 = -b / 2 * a;
+            x2 = x1;
+            System.out.println("Funkcija ima jedno rjesenje x1=x2=" + x1);
+        }
+
+        if (d > 0) {
+            x1 = (-b + Math.sqrt(d)) / 2 * a;
+            x2 = (-b - Math.sqrt(d)) / 2 * a;
+        }
+        System.out.println("Funkcija ima 2 rjesenja: ");
+        System.out.println("x1=" + x1 +"   " + "x2=" + x2);
+
+    }
+}
